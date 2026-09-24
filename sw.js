fd573153dc0c6e088f849e12e71c6d3c4ecf157f
n9rk79625t-droid <n9rk79625t@privaterelay.appleid.com>
2026-09-23 21:31:35 -0500

    Fix stale PWA cache and activate updates immediately

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE="celestial-within-v5";
-const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{let copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match("./index.html"))))});
+const CACHE="celestial-within-v6";
+const CORE=["./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener("fetch",e=>{
+ if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).then(resp=>{let copy=resp.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return resp}).catch(()=>caches.match("./index.html")));return}
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{let copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp})));
+});
